47c68c39171f05c27febe502451122f97164816a
tanvirul15 <tanvirul15-6117@diu.edu.bd>
2021-02-27 16:28:46 +0600

    Partially Completed..

---
+++ src/component/Product/Product.js
@@ -0,0 +1,34 @@
+import React from "react";
+import "./product.css";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faShoppingCart } from "@fortawesome/free-solid-svg-icons";
+
+const Product = (props) => {
+  const { img, name, seller, price, stock } = props.product;
+  console.log(props);
+  return (
+    <div className='product'>
+      <div>
+        <img src={img} alt='' />
+      </div>
+
+      <div>
+        <h4 className='product-name'>Name: {name}</h4>
+        <p>
+          <small>by: {seller}</small>
+        </p>
+        <p>$ {price}</p>
+        <br />
+        <p>
+          <small>Only {stock} left in stock</small>
+        </p>
+        <button className='cartBtn' onClick={() => props.handleAddProduct()}>
+          <FontAwesomeIcon icon={faShoppingCart} />
+          &nbsp;Add to Cart
+        </button>
+      </div>
+    </div>
+  );
+};
+
+export default Product;
---
+++ src/component/Shop/Shop.js
@@ -0,0 +1,29 @@
+import React, { useState } from "react";
+import "./shop.css";
+import fakeData from "../../fakeData/";
+import Product from "../Product/Product";
+
+const Shop = () => {
+  const first10 = fakeData.slice(0, 10);
+  console.log(first10);
+  const [products, setProducts] = useState(first10);
+
+  const addProduct = () => {
+    console.log("Product Added..!");
+  };
+
+  return (
+    <div className='shop-container'>
+      <div className='product-container'>
+        {first10.map((item) => (
+          <Product product={item} handleAddProduct={addProduct}></Product>
+        ))}
+      </div>
+      <div className='cart-container'>
+        <h1>Cart Container</h1>
+      </div>
+    </div>
+  );
+};
+
+export default Shop;
